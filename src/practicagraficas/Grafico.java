807c60cf158f00697fe409c92666e195c61777e4
StrangerPlay17 <132041390+StrangerPlay17@users.noreply.github.com>
2024-04-18 17:37:58 -0600

    Class Grafico Updated

--- src/practicagraficas/Grafico.java
+++ src/practicagraficas/Grafico.java
@@ -23,7 +23,7 @@ public class Grafico {
 
         // Se crea un nuevo gráfico de barras
         chart = ChartFactory.createBarChart(
-                "Planificador Round Robin",
+                "Planificador MLQ",
                 "ID Procesos",
                 "Tiempo",
                 dataset
